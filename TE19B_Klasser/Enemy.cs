38f813f62ba767b729fd0be03ac967cf1cf2570d
Mikael Bergström <krank23@gmail.com>
2021-08-26 11:09:40 +0200

    Initial commit

---
+++ TE19B_Klasser/Enemy.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TE19B_Klasser
+{
+  public class Enemy
+  {
+    public int x;
+    public int y;
+
+    public string name;
+
+    public int health;
+    public int damage;
+  }
+}
---
+++ TE19B_Klasser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TE19B_Klasser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Enemy goomba = new Enemy();
+
+      goomba.name = "Goomba";
+      goomba.health = 100;
+      goomba.health -= 8;
+      goomba.health += 3;
+
+      Console.WriteLine($"Name: {goomba.name} Health: {goomba.health}");
+
+      Console.ReadLine();
+      
+    }
+  }
+}
